51ee431133b1dc06c816533903ed9262e43ab77a
amari <qupunto@gmail.com>
2025-02-26 20:10:45 +0100

    merge

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,5 +1,4 @@
-<<<<<<< Updated upstream
-﻿import {Animal} from "./animal.js"
+﻿﻿import {Animal} from "./animal.js"
 import {constants} from "./constants.js";
 import {SolidRectangle} from "./solidRectangle.js";
 
@@ -10,19 +9,6 @@ export class Game {
         this.canvas.height = Math.max(window.innerHeight, constants.min_height);
         this.canvas.width = Math.max(window.innerWidth, constants.min_width);
         this.animal = new Animal(animal, this.canvas.width / 2 - animal.size, this.canvas.height - 50);
-=======
-﻿import { default as Animal } from "./animal.js"
-import { default as Constants } from "./constants.js"
-export default class Game {
-    constructor(canvasId) {
-        this.constants = new Constants();
-        this.canvas = document.getElementById(canvasId);
-        this.ctx = this.canvas.getContext('2d');
-        this.canvas.height = Math.max(window.innerHeight, this.constants.min_height);
-        this.canvas.width = Math.max(window.innerWidth, this.constants.min_width);
-        this.animal = {};
-        this.direction = {};
->>>>>>> Stashed changes
         this.keysPressed = {};
         this.bindKeys();
         this.gameLoop = this.gameLoop.bind(this);
@@ -46,16 +32,8 @@ export default class Game {
         });
         document.addEventListener('keyup', (event) => {
             this.keysPressed[event.code] = false;
-<<<<<<< Updated upstream
             if (!this.keysPressed["ArrowLeft"] && !this.keysPressed["ArrowRight"])
                 this.animal.noKey();
-=======
-
-            if (this.keysPressed["ArrowLeft"]) this.direction.x = this.constants.direction.LEFT;
-            else if (this.keysPressed["ArrowRight"]) this.direction.x = this.constants.direction.RIGHT;
-            else this.direction.x = null;
-
->>>>>>> Stashed changes
         });
     }
     new() {
@@ -63,11 +41,7 @@ export default class Game {
         this.gameState = constants.gameStates.SELECTING;
     }
     update() {
-<<<<<<< Updated upstream
         this.animal.update(this.terrain);
-=======
-        this.animal.update(this.direction?.x);
->>>>>>> Stashed changes
     }
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -81,16 +55,9 @@ export default class Game {
         this.draw();
         requestAnimationFrame(this.gameLoop);
     }
-<<<<<<< Updated upstream
 
     start() {
         this.gameState = constants.gameStates.PLAYING;
-=======
-    start(type, animal) {
-        this.gameState = this.constants.gameStates.PLAYING;
-        this.animal = new Animal(animal, this.canvas, this.ctx);
-        this.gameType = type;
->>>>>>> Stashed changes
         this.gameLoop();
     }
     #createTerrain() {
