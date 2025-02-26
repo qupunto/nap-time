3a1f8b3040ddb11eb138b0e5bb514c9826f2bdee
amari <qupunto@gmail.com>
2025-02-26 20:08:04 +0100

    merge

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 export const constants = Object.freeze({
     full_stop: 4,
     gravity: 0.98,
@@ -56,3 +57,62 @@ export const constants = Object.freeze({
         }
     },
 });
+=======
+export default class Constants{
+    constructor(){
+        this.full_stop=5;
+        this.gravity=0.98;
+        this.idle_counter=500;
+        this.min_height=360;
+        this.min_width=480;
+        this.framerate=1000/24;
+        this.gameStates=Object.freeze({
+            SELECTING: "selecting",
+            PLAYING: "playing",
+            PAUSED:"paused",
+            OVER: "over"
+        });
+        this.gameTypes=Object.freeze({
+            FEED: "feed",
+            PLAY: "play",
+            PAT: "pat"
+        });
+        this.animalStates=Object.freeze({
+            JUMPING: "jumping",
+            DOUBLE_JUMPING: "double_jumping",
+            SLAMMING: "slamming",
+            RESTING: "resting",
+            EATING: "eating",
+            MOVING: "moving",
+            IDLE: "idle",
+            PLAYING: "playing"
+        });
+        this.direction=Object.freeze({
+            UP: "up",
+            DOWN: "down",
+            LEFT: "left",
+            RIGHT: "right"
+        });
+        this.animalTypes=Object.freeze({
+            "NIT": {
+                max_speed: 10,
+                acceleration: 0.4,
+                type: "cat",
+                size: 50,
+                jumping_power: 18,
+                name: "Nit",
+                color: "gray"
+            },
+            "DIA": {
+                max_speed: 15,
+                acceleration: 0.5,
+                type: "cat",
+                size: 25,
+                jumping_power: 22,
+                name: "Dia",
+                color: "white"
+            }
+        });
+    }
+}
+>>>>>>> Stashed changes
--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,7 +1,16 @@
+<<<<<<< Updated upstream
 ﻿import { Game } from "./game.js"
 import { constants } from "./constants.js";
 
 window.addEventListener('load', () => {
   const game = new Game('gameCanvas', constants.animalTypes.NIT);
   game.start();
+=======
+﻿import { default as Game } from "./game.js"
+import { default as Constants } from "./constants.js"
+window.addEventListener('load', () => {
+  const game = new Game('gameCanvas');
+  const constants = new Constants();
+  game.start(constants.gameTypes.FEED, constants.animalTypes.DIA);
+>>>>>>> Stashed changes
 });
